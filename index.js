eac8bdc27d7604d16a0cc377498fd939d5d2355f
Carlo Santos <carlox7@gmail.com>
2018-02-03 13:42:52 -0800

    fix: removed public folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(`${__dirname}/public`));
-app.get('*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+app.use(express.static(`${__dirname}`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
 app.listen(4000, () => console.log('Pixel App on port 4000!'));
